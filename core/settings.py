56cc0a4ba72716b62b9bc51a6056793fe5c33434
Mohammad-seyfi <149678843+hypnotize1@users.noreply.github.com>
2025-08-01 17:54:26 +0330

    change database to postgres

--- core/settings.py
+++ core/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'myshopdb',
+        'USER':'Mohammad',
+        'PASSWORD':'MYpassword@1380',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
