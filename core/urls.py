ff7fb82d415091f99f0e9e37822d867e77f5fd85
Mohammad-seyfi <149678843+hypnotize1@users.noreply.github.com>
2025-08-02 18:14:22 +0330

    add accounts and shop and third-party packages[drf-spectacular, simple-jwt]

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_spectacular.views import SpectacularRedocView, SpectacularSwaggerView, SpectacularAPIView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Local apps urls 
+    path('shop/', include('shop.urls', namespace='shop')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    # JWT urls
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Schema urls 
+    path('schema/', SpectacularAPIView.as_view(), name='schema'), 
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+                                                                                         
 ]
