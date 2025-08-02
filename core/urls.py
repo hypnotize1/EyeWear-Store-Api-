deb39cd1dee5f759bf02b913012e13bf21378f57
Mohammad-seyfi <149678843+hypnotize1@users.noreply.github.com>
2025-08-02 18:26:37 +0330

    update comments for better readability

--- core/urls.py
+++ core/urls.py
@@ -28,8 +28,9 @@ urlpatterns = [
     # JWT urls
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # Schema urls 
-    path('schema/', SpectacularAPIView.as_view(), name='schema'), 
+    # main schema (JSON format)
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    # UI for schema 
     path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
                                                                                          
