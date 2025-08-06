576dbed72777668e1fceae8af34e3dea52dd3675
Mohammad-seyfi <149678843+hypnotize1@users.noreply.github.com>
2025-08-06 17:28:56 +0330

    register and customize custom user model in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from accounts.models import CustomUser
 
-# Register your models here.
+# Register your models here
+# .
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    list_display = ['email', 'name', 'phone','is_staff', 'is_active']
+    list_filter = ['is_staff', 'is_active', 'is_superuser']
+    search_fields = ['email', 'name', 'phone']
+    ordering = ('-date_joined',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name', 'phone')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'phone', 'password1', 'password2'),
+        }),
+    )
+
